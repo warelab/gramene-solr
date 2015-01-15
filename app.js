1e521b7c954015459fa2cc0e1d15c2ef7615b960
Andrew Olson <olson@cshl.edu>
2015-01-15 12:11:57 -0500

    added post method

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express  = require('express'),
     request = require('request'),
     qs = require('querystring'),
     cors = require('cors'),
+    bodyParser = require('body-parser'),
     validate = require('conform').validate;
 
 // load settings from config file
@@ -11,6 +12,9 @@ var settings = require('./config/settings.json');
 var app = express();
 
 app.use(cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 var port = process.argv.length > 2 ? process.argv[2] : 8043;
 
@@ -48,6 +52,21 @@ app.get('/:core', function (req, res, next) {
       + '/solr/' + c + '/select?wt=json&indent=true&'
       + qs.stringify(req.query);
     request.get(url).pipe(res);
+  }
+});
+
+app.post('/:core', function (req, res, next) {
+  // check if the collection exists
+  var c = req.params.core;
+  if (!cores.hasOwnProperty(c)) res.json({"error":" '"+c+"' not found"});
+  else {
+    // consider validation of query params to secure solr...
+    var url = settings.hostname
+      + ':' + settings.port
+      + '/solr/' + c + '/select';
+    req.body.wt = 'json';
+    req.body.indent = true;
+    request.post(url,{form:req.body}).pipe(res);
   }
 });
 
