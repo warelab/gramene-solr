e194c3fc097db54f96f7c7688c2a7fba196af450
Andrew Olson <olson@cshl.edu>
2015-04-24 12:47:01 -0400

    adding suggest route

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var express  = require('express'),
     bodyParser = require('body-parser'),
     validate = require('conform').validate;
 
-// load settings from config file
-var settings = require('./config/settings.json');
-
 var app = express();
 
 app.use(cors());
@@ -16,7 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-var port = process.argv.length > 2 ? process.argv[2] : 8043;
+var port = process.argv[2];
+var solrURL = process.argv[3];
 
 var cores = {
   genes : {
@@ -41,29 +39,40 @@ app.get('/', function (req, res, next) { // return top level info
   res.json(cores);
 });
 
-app.get('/:core', function (req, res, next) {
+app.get('search/:core', function (req, res, next) {
   // check if the collection exists
   var c = req.params.core;
   if (!cores.hasOwnProperty(c)) res.json({"error":" '"+c+"' not found"});
   else {
     // consider validation of query params to secure solr...
-    var url = settings.hostname
-      + ':' + settings.port
-      + '/solr/' + c + '/select?wt=json&indent=true&'
+    var url = solrURL
+      + '/solr/' + c + '/query?'
       + qs.stringify(req.query);
     request.get(url).pipe(res);
   }
 });
 
-app.post('/:core', function (req, res, next) {
+app.get('suggest/:core', function (req, res, next) {
   // check if the collection exists
   var c = req.params.core;
   if (!cores.hasOwnProperty(c)) res.json({"error":" '"+c+"' not found"});
   else {
     // consider validation of query params to secure solr...
-    var url = settings.hostname
-      + ':' + settings.port
-      + '/solr/' + c + '/select';
+    var url = solrURL
+      + '/solr/' + c + '/suggest?indent=true&wt=json&'
+      + qs.stringify(req.query);
+    request.get(url).pipe(res);
+  }
+});
+
+app.post('search/:core', function (req, res, next) {
+  // check if the collection exists
+  var c = req.params.core;
+  if (!cores.hasOwnProperty(c)) res.json({"error":" '"+c+"' not found"});
+  else {
+    // consider validation of query params to secure solr...
+    var url = solrURL
+      + '/solr/' + c + '/query';
     req.body.wt = 'json';
     req.body.indent = true;
     request.post(url,{form:req.body}).pipe(res);
