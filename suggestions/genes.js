5ca7f6a79dc8f182dc8fd89199c7ff3a38464906
Andrew Olson <olson@cshl.edu>
2016-01-05 12:41:04 -0500

    added genetree and biotype suggestions

--- suggestions/genes.js
+++ suggestions/genes.js
@@ -42,75 +42,118 @@ collections.genes.mongoCollection().then(function(collection) {
           console.log(JSON.stringify(solr));
         }
       }
-      // read the mongo genes docs and generate suggestions with unique values only
-      require('readline').createInterface(
-        {
-          input: process.stdin,
-          terminal: false
-        }
-      ).on('line', function(line) { // one JSON object per line
-        var mongo = JSON.parse(line);
-
-        console.log(',');
-        console.log(JSON.stringify({
-          category : 'Gene',
-          fq_field : 'id',
-          fq_value : mongo._id,
-          id       : mongo._id,
-          display_name : mongo._id,
-          num_genes : 1,
-          relevance : 0,
-          taxon_id : mongo.taxon_id
-        }));
-
-        // add uniquely identifying xrefs
-        var xref_h = {};
-        for (var db in mongo.xrefs) {
-          if (!collections.hasOwnProperty(db)) {
-            mongo.xrefs[db].forEach(function(xr) {
-              xref_h[xr]=db;
-            });
-          }
-        }
-        Object.keys(xref_h).filter(function(xr) {
-          return !term_freq[xr.toUpperCase()];
-        }).forEach(function(xr) {
+      
+      // gene tree suggestion
+      var url = genesURL + '/query?rows=0&facet=true&facet.limit=-1&json.nl=map&facet.mincount=1&facet.field=grm_gene_tree';
+      console.error(url);
+      http.read(url).then(function(data) {
+        var tree_size = JSON.parse(data).facet_counts.facet_fields.grm_gene_tree;
+        for (var tree in tree_size) {
+          var solr = {
+            category    : 'Gene tree',
+            id          : '_term_'+ ++n,
+            display_name: tree,
+            name        : tree,
+            fq_field    : 'grm_gene_tree',
+            fq_value    : tree,
+            num_genes   : tree_size[tree],
+            relevance   : 1
+          };
           console.log(',');
-          console.log(JSON.stringify({
-            category : 'Gene',
-            fq_field : 'id',
-            fq_value : mongo._id,
-            id       : '_term_'+ ++n,
-            xref     : xr,
-            display_name : xr,
-            num_genes : 1,
-            relevance : 0,
-            taxon_id : mongo.taxon_id
-          }));
-        });
+          console.log(JSON.stringify(solr));
+        }
 
-        // add uniquely identifying synonyms
-        if (mongo.hasOwnProperty('synonyms')) {
-          mongo.synonyms.filter(function(syn) {
-            return !term_freq[syn.toUpperCase()];
-          }).forEach(function(syn) {
+        // biotype suggestion
+        var url = genesURL + '/query?rows=0&facet=true&facet.limit=-1&json.nl=map&facet.mincount=1&facet.field=biotype';
+        console.error(url);
+        http.read(url).then(function(data) {
+          var biotype_count = JSON.parse(data).facet_counts.facet_fields.biotype;
+          for (var biotype in biotype_count) {
+            var solr = {
+              category    : 'Biotype',
+              id          : '_term_'+ ++n,
+              display_name: biotype,
+              name        : biotype,
+              fq_field    : 'biotype',
+              fq_value    : biotype,
+              num_genes   : biotype_count[biotype],
+              relevance   : 1
+            };
+            console.log(',');
+            console.log(JSON.stringify(solr));
+          }
+      
+          // read the mongo genes docs and generate suggestions with unique values only
+          require('readline').createInterface(
+            {
+              input: process.stdin,
+              terminal: false
+            }
+          ).on('line', function(line) { // one JSON object per line
+            var mongo = JSON.parse(line);
+
             console.log(',');
             console.log(JSON.stringify({
               category : 'Gene',
               fq_field : 'id',
               fq_value : mongo._id,
-              id       : '_term_'+ ++n,
-              synonym  : syn,
-              display_name : syn,
+              id       : mongo._id,
+              display_name : mongo._id,
               num_genes : 1,
               relevance : 0,
               taxon_id : mongo.taxon_id
             }));
-          });
-        }
 
-      }).on('close', function() {
-        console.log(']');
+            // add uniquely identifying xrefs
+            var xref_h = {};
+            for (var db in mongo.xrefs) {
+              if (!collections.hasOwnProperty(db)) {
+                mongo.xrefs[db].forEach(function(xr) {
+                  xref_h[xr]=db;
+                });
+              }
+            }
+            Object.keys(xref_h).filter(function(xr) {
+              return !term_freq[xr.toUpperCase()];
+            }).forEach(function(xr) {
+              console.log(',');
+              console.log(JSON.stringify({
+                category : 'Gene',
+                fq_field : 'id',
+                fq_value : mongo._id,
+                id       : '_term_'+ ++n,
+                xref     : xr,
+                display_name : xr,
+                num_genes : 1,
+                relevance : 0,
+                taxon_id : mongo.taxon_id
+              }));
+            });
+
+            // add uniquely identifying synonyms
+            if (mongo.hasOwnProperty('synonyms')) {
+              mongo.synonyms.filter(function(syn) {
+                return !term_freq[syn.toUpperCase()];
+              }).forEach(function(syn) {
+                console.log(',');
+                console.log(JSON.stringify({
+                  category : 'Gene',
+                  fq_field : 'id',
+                  fq_value : mongo._id,
+                  id       : '_term_'+ ++n,
+                  synonym  : syn,
+                  display_name : syn,
+                  num_genes : 1,
+                  relevance : 0,
+                  taxon_id : mongo.taxon_id
+                }));
+              });
+            }
+
+          }).on('close', function() {
+            console.log(']');
+          });
+        });
       });
     });
   });
